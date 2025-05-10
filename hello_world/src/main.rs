0e44660074044218a25f77f0dda01fbbe2510f57
phlucasfr <phlucasfr@gmail.com>
2025-05-10 19:15:14 -0300

    1.2. Hello, World!

---
+++ hello_world/src/main.rs
@@ -0,0 +1,6 @@
+//Main é uma função especial que é sempre o primeiro código que roda em todos os programas Rust.
+//Rust requer chaves ao redor do corpo da função
+fn main() {
+    //println! é uma macro que recebe uma string como argumento e imprime a string na tela.
+    println!("Hello, world!"); // ';' Indica que uma expressão acabou.
+}
