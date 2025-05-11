a4db960c2b0d0f6ca6c835fa94ae3a2143cfd2f2
phlucasfr <phlucasfr@gmail.com>
2025-05-11 09:19:15 -0300

    2. Programming a Guessing Game - Listing 2-6

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,20 +1,43 @@
 use rand::Rng;
-use std::io;
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u8 = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    println!("Please input a guess between 1 and 100!");
 
-    println!("Please input your guess!");
+    loop {
+        let mut guess: String = String::new();
 
-    let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => {
+                if !(1..=100).contains(&num) {
+                    println!("Please enter a number between 1 and 100");
+                    continue;
+                }
+                num
+            }
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Wou win!");
+                break;
+            }
+        }
+    }
 }
