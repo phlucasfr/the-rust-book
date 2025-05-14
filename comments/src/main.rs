6855aa3140a774e63030054437a0c8c024f68110
phlucasfr <phlucasfr@gmail.com>
2025-05-14 10:42:59 -0300

    3.4. Comments

---
+++ comments/src/main.rs
@@ -0,0 +1,9 @@
+// So we’re doing something complicated here, long enough that we need
+// multiple lines of comments to do it! Whew! Hopefully, this comment will
+// explain what’s going on.
+
+fn main() {
+    // I’m feeling lucky today
+    let lucky_number = 7;
+    println!("The lucky numer is {}!", lucky_number); // I’m feeling lucky today
+}
