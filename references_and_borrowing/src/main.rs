6f0efbeb787d12a5331bcd2437dd5fdf84cbc6b3
phlucasfr <phlucasfr@gmail.com>
2025-05-19 22:24:12 -0300

    4.2. References and Borrowing - cannot borrow `s` as mutable more than once at a time example

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -1,11 +1,20 @@
 fn main() {
-    let s1 = String::from("hello");
+    let mut s1 = String::from("hello");
     let len = calculate_length(&s1);
 
-    println!("The length of '{s1}' is {len}")
+    println!("The length of '{s1}' is {len}");
 
     //invalid operation cannot borrow `*some_string` as mutable, as it is behind a `&` reference
-    // change(&s1);
+
+    change(&mut s1);
+
+    //cannot borrow `s` as mutable more than once at a time example
+    // let mut s = String::from("hello");
+
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+
+    // println!("{}, {}", r1, r2);
 }
 
 fn calculate_length(s: &String) -> usize {
@@ -16,3 +25,7 @@ fn calculate_length(s: &String) -> usize {
 // fn change(some_string: &String) {
 //     some_string.push(", world");
 // }
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
