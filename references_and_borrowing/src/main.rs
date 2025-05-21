57348fd7261ac08d20bc0ed05a992385c1b78db0
phlucasfr <phlucasfr@gmail.com>
2025-05-21 09:03:16 -0300

    4.2. References and Borrowing - end

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -15,7 +15,40 @@ fn main() {
     // let r2 = &mut s;
 
     // println!("{}, {}", r1, r2);
+
+    let mut s = String::from("hello");
+
+    {
+        let r1 = &mut s;
+        println!("the value of s is {r1}");
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let r2 = &mut s;
+    println!("the value of s is {r2}");
+
+    //cannot borrow `s` as mutable because it is also borrowed as immutable
+    // let r1 = &s; // no problem
+    // let r2 = &s; // no problem
+    // let r3 = &mut s; // BIG PROBLEM
+    // println!("{}, {}, and {}", r1, r2, r3);
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{r1} and {r2}");
+    // Variables r1 and r2 will not be used after this point.
+
+    let r3 = &mut s; // no problem
+    println!("{r3}");
 }
+
+//lifetime missing example, this function's return type contains a borrowed value, but there is no value
+//for it to be borrowed from
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+
+//     &s
+// }// Here, s goes out of scope, and is dropped, so its memory goes away.
+// Danger!
 
 fn calculate_length(s: &String) -> usize {
     s.len()
