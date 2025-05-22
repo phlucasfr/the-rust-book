d3a617444afa071ed51f291c215a1acdb5c4f140
phlucasfr <phlucasfr@gmail.com>
2025-05-22 19:40:30 -0300

    4.3. The Slice Type - let’s rewrite first_word to return a slice

--- slice_type/src/main.rs
+++ slice_type/src/main.rs
@@ -1,7 +1,24 @@
 fn main() {
     //string slice example
-    let s = String::from("hello world");
+    let mut s = String::from("hello world");
 
-    let hello = &s[0..5];
-    let world = &s[6..11];
+    // let hello = &s[0..5];
+    // let world = &s[6..11];
+
+    let word = first_word(&s);
+    // s.clear(); // error!
+
+    println!("the first word is {word}")
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
