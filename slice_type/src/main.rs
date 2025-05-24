ccb6f8aa44cabd924ffc052d3e4c2ee586c134a2
phlucasfr <phlucasfr@gmail.com>
2025-05-24 17:30:36 -0300

    4.3. The Slice Type - end

--- slice_type/src/main.rs
+++ slice_type/src/main.rs
@@ -8,10 +8,39 @@ fn main() {
     let word = first_word(&s);
     // s.clear(); // error!
 
-    println!("the first word is {word}")
+    println!("the first word is {word}");
+
+    let my_string = String::from("hello world");
+
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole.
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s.
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or
+    // whole.
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
 }
 
-fn first_word(s: &String) -> &str {
+//"fn first_word(s: &str) -> &str {" allows us to use the same function on both &String values and &str values.
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
