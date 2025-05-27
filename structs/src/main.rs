1008fc9088d5709644162a90735acc38a63cc530
phlucasfr <phlucasfr@gmail.com>
2025-05-27 09:29:08 -0300

    5.1. Defining and Instantiating Structs - Listing 5-7: Using struct update syntax to set a new email value for a User instance but to use the rest of the values from user1

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -14,6 +14,20 @@ fn main() {
     };
 
     user1.email = String::from("anotheremail@example.com");
+
+    //without update sintax
+    // let user2 = User {
+    //     active: user1.active,
+    //     username: user1.username,
+    //     email: String::from("another@example.com"),
+    //     sign_in_count: user1.sign_in_count,
+    // };
+
+    //using update sintax
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
 }
 
 fn build_user(email: String, username: String) -> User {
