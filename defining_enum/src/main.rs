0334ba49b44f802890686815a08efc77002b526f
phlucasfr <phlucasfr@gmail.com>
2025-06-08 11:07:14 -0300

    6.1. Defining an Enum - Listing 6-1: Storing the data and IpAddrKind variant of an IP address using a struct

---
+++ defining_enum/src/main.rs
@@ -0,0 +1,36 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(four);
+    route(six);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    dbg!(home);
+    dbg!(loopback);
+}
+
+fn route(ip_kind: IpAddrKind) {
+    println!("The kind of ip is {:?}", ip_kind);
+}
