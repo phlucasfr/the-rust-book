f701aa5a1ee0217180b36ff457a84a346f54cd3d
phlucasfr <phlucasfr@gmail.com>
2025-06-10 08:42:34 -0300

    6.1. Defining an Enum - Listing 6-2: A Message enum whose variants each store different amounts and types of values

--- defining_enum/src/main.rs
+++ defining_enum/src/main.rs
@@ -4,10 +4,23 @@ enum IpAddrKind {
     V6,
 }
 
+// #[derive(Debug)]
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
 #[derive(Debug)]
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32, i32),
 }
 
 fn main() {
@@ -17,15 +30,8 @@ fn main() {
     route(four);
     route(six);
 
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let home = IpAddr::V4(127, 0, 0, 8);
+    let loopback = IpAddr::V6(String::from("::1"));
 
     dbg!(home);
     dbg!(loopback);
