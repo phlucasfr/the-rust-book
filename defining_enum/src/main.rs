3a37f7bcc2c3d9d2387c9214356ff5f68456265b
phlucasfr <phlucasfr@gmail.com>
2025-06-11 23:35:46 -0300

    6.1. Defining an Enum - Option<T> | Some<T>|  None | match - End

--- defining_enum/src/main.rs
+++ defining_enum/src/main.rs
@@ -23,6 +23,19 @@ enum Message {
     ChangeColor(i32, i32, i32, i32),
 }
 
+impl Message {
+    fn call(&self) {}
+}
+
+struct QuitMessage; //unit struct
+struct MoveMessage {
+    //standard struct
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); //tuple struct
+struct ChangeColorMessage(i32, i32, i32); //tuple struct
+
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
@@ -35,6 +48,27 @@ fn main() {
 
     dbg!(home);
     dbg!(loopback);
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number: Option<i32> = Some(5);
+    let some_char: Option<char> = Some('e');
+
+    let absent_number: Option<i32> = None; //require type annotation
+
+    let x: i8 = 5;
+    let mut y: Option<i8> = Some(10);
+
+    //cannot add `Option<i8>` to `i8`
+    // let sum = x + y;
+
+    let sum = x + match y.as_mut() {
+        Some(val) => *val,
+        None => 0,
+    };
+
+    println!("The sum of x and y is {}", sum);
 }
 
 fn route(ip_kind: IpAddrKind) {
