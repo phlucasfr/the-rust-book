f808f309b9593c77968ec7791ff93a483b685c8e
Phelipe <phlucasfr@gmail.com>
2025-06-15 08:35:52 -0300

    6.2. The match Control Flow Construct - end

--- match_control_flow/src/main.rs
+++ match_control_flow/src/main.rs
@@ -33,10 +33,48 @@ fn plus_one(x: Option<i32>) -> Option<i32> {
     }
 }
 
+// non-exhaustive patterns: `None` not covered
+// fn plus_one(x: Option<i32>) -> Option<i32> {
+//     match x {
+//         Some(i) => Some(i + 1),
+//     }
+// }
+
+
 fn main() {
     value_in_cents(Coin::Quarter(UsState::Alaska));
 
     let five = Some(5);
     let six = plus_one(five);
     let none = plus_one(None);
+
+    let dice_roll = 9;
+
+    // catch-all pattern
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+    }
+
+    // catch-all pattern without catch-all
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => reroll(),
+    }
+
+    // catch-all pattern without actions by using empty tuple type
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
+    }
+
 }
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
+fn reroll() {}
+
