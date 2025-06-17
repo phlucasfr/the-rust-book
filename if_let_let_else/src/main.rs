1e587c41542f07b95120a6cff9ab8755bbefc9e2
phlucasfr <phlucasfr@gmail.com>
2025-06-17 09:06:59 -0300

    6.3. Concise Control Flow with if let and let else - Listing 6-7: Checking whether a state existing in 1900 by using conditionals nested inside an if let.

--- if_let_let_else/src/main.rs
+++ if_let_let_else/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 enum Coin {
     Penny,
     Nickel,
@@ -11,15 +13,24 @@ enum UsState {
     Alaska,
 }
 
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool {
+        match self {
+            UsState::Alabama => year >= 1819,
+            UsState::Alaska => year >= 1959,
+        }
+    }
+}
+
 fn main() {
     let config_max = Some(3u8);
     match config_max {
         Some(max) => println!("The maximum is configured to be {max}"),
-        _ => ()
+        _ => (),
     }
 
     // Shorter way using if let
-    if let Some(max) = config_max  {
+    if let Some(max) = config_max {
         println!("The maximum is configured to be {max}");
     }
 
@@ -37,5 +48,17 @@ fn main() {
         println!("State quarter from {state:?}");
     } else {
         count += 1;
+    }
+}
+
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    if let Coin::Quarter(state) = coin {
+        if state.existed_in(1900) {
+            Some(format!("{state:?} is pretty old, for America!"))
+        } else {
+            Some(format!("{state:?} is relatively new!"))
+        }
+    } else {
+        None
     }
 }
