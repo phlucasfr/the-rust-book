498153cc2d377a16090293d60ea73c5648d287b1
phlucasfr <phlucasfr@gmail.com>
2025-06-20 07:22:55 -0300

    7.2. Defining Modules to Control Scope and Privacy - Now let’s get into the details of these rules and demonstrate them in action!

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,4 +1,15 @@
-// crate root 
+// crate root
+use crate::garden::vegetables::Asparagus;
+
+pub mod garden;
+
 fn main() {
-    println!("Hello, world!");
+    let plant = Asparagus {};
+    println!("I'm growing {plant:?}!")
+}
+
+// inline module
+mod animal {
+    // inline submodule
+    mod cat {}
 }
