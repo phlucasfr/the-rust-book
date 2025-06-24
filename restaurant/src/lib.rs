b48d9dcd04768ec8331ed9483fe84582aeee8106
phlucasfr <phlucasfr@gmail.com>
2025-06-24 10:42:32 -0300

    7.3. Paths for Referring to an Item in the Module Tree - Listing 7-9: A struct with some public fields and some private fields

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,17 +1,33 @@
 mod front_of_house {
     pub mod hosting {
-        pub fn add_to_waitlist(){}
-        fn seat_at_table(){}
+        pub fn add_to_waitlist() {}
+        fn seat_at_table() {}
     }
 
     mod serving {
-        fn take_order(){}
-        fn serve_order(){}
-        fn take_payment(){}        
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
     }
 }
 
 mod back_of_house {
+    use std::string;
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::deliver_order();
@@ -24,8 +40,11 @@ fn deliver_order() {}
 
 pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
-}
 
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast)
+}
 
 // crate
 //  └── front_of_house
