e01490832e29c494020ddcc619889acaf9015cbe
Phelipe <phlucasfr@gmail.com>
2025-07-02 10:02:12 -0300

    7.5. Separating Modules into Different Files - end

--- restaurant/src/front_of_house.rs
+++ restaurant/src/front_of_house.rs
@@ -1,10 +1,1 @@
-pub mod hosting {
-    pub fn add_to_waitlist() {}
-    fn seat_at_table() {}
-}
-
-mod serving {
-    fn take_order() {}
-    fn serve_order() {}
-    fn take_payment() {}
-}
+pub mod hosting;
