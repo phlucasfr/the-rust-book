5f7afc0bbc1c819a054b2411a8af6548c5b43863
Phelipe <phlucasfr@gmail.com>
2025-07-04 10:27:02 -0300

    8.1. Storing Lists of Values with Vectors - Listing 8-4: Using indexing syntax and using the get method to access an item in a vector

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 fn main() {
     let mut v: Vec<i32> = Vec::new();
     v.push(0);
@@ -6,4 +8,14 @@ fn main() {
     v.push(4);
     v.push(5);
     v.push(6);
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2); 
+    match third {
+        Some(num) => println!("The third element is {num}"),
+        None => println!("There's no third element")
+    }
+
 }
