bca101ffe422fb34447546411cd4b6d8c86d8401
Phelipe <phlucasfr@gmail.com>
2025-07-05 18:46:19 -0300

    8.1. Storing Lists of Values with Vectors - Listing 8-6: Attempting to add an element to a vector while holding a reference to an item

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -18,4 +18,11 @@ fn main() {
         None => println!("There's no third element")
     }
 
+    let mut v = vec![1, 2, 3, 4, 5];
+    let first = &v[0];
+
+    // cannot borrow `v` as mutable because it is also borrowed as immutable
+    // v.push(6);
+    println!("The first element is: {first}");
+
 }
