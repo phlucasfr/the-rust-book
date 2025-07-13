102705c6e0907daf2d47679532a8064a0548ed85
Phelipe <phlucasfr@gmail.com>
2025-07-13 08:49:21 -0300

    8.3. Storing Keys with Associated Values in Hash Maps - Listing 8-21: Accessing the score for the Blue team stored in the hash map

--- hash_map/src/main.rs
+++ hash_map/src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 500);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    println!("the {} score is: {}", team_name, score);
 }
