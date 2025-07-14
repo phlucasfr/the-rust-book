9f7c88d2b4d3c4eaacc4bd7291fa6c1cedcd0ebf
Phelipe <phlucasfr@gmail.com>
2025-07-14 08:43:58 -0300

    8.3. Storing Keys with Associated Values in Hash Maps - Listing 8-22: Showing that keys and values are owned by the hash map once they’re inserted

--- hash_map/src/main.rs
+++ hash_map/src/main.rs
@@ -10,4 +10,18 @@ fn main() {
     let score = scores.get(&team_name).copied().unwrap_or(0);
 
     println!("the {} score is: {}", team_name, score);
+
+    for (key, value) in &scores  {
+        println!("key: {}, value: {}", key, value)
+    }
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    // field_name and field_value are invalid at this point
+    // println!("{}", field_name); borrow of moved value: `field_name`
 }
+
