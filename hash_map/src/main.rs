4844810e8a8937ba73f1750c478383bddd0a4e0f
Phelipe <phlucasfr@gmail.com>
2025-08-26 10:05:11 -0300

    8.3. Storing Keys with Associated Values in Hash Maps - Listing 8-24: Using the entry method to only insert if the key does not already have a value

--- hash_map/src/main.rs
+++ hash_map/src/main.rs
@@ -6,6 +6,9 @@ fn main() {
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 500);
 
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
     let team_name = String::from("Blue");
     let score = scores.get(&team_name).copied().unwrap_or(0);
 
